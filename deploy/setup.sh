ad067de062adfae791e22c638f38230535176c40
Ben Brunton <ben@flashtalking.com>
2013-09-13 23:40:17 +0100

    include tsc compile in deployment

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,4 +3,10 @@
 echo "setting up Hollywood Hacking - build # ""$TRAVIS_BUILD_NUMBER""\n"
 
 npm install bower
+npm install typescript
 bower install
+
+cd script/app/
+dir *.ts /b /s > ts-files.txt
+tsc @ts-files.txt --module amd
+del ts-files.txt
