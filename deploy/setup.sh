3ab6668ee02f4c9c13b72a7bd0ebbe9ac54c0c23
benbrunton <ben@flashtalking.com>
2013-09-13 14:47:30 +0100

    some CI tests

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo "setting up Hollywood Hacking - build # ""$TRAVIS_BUILD_NUMBER"
+ssh -T git@github.com
 npm install bower
 bower install
 echo "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
