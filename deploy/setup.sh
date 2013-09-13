d8b8e01cda15e4f48c6a00de9b416ea648b720d0
benbrunton <ben@flashtalking.com>
2013-09-13 15:22:01 +0100

    fucker fucker fucker

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,11 +3,8 @@
 echo "setting up Hollywood Hacking - build # ""$TRAVIS_BUILD_NUMBER""\n"
 
 echo "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-#chmod 600 id_travis.pub.pem
-#ssh-add id_travis.pub.pem
-chmod 600 id_travis.pub
-ssh-add id_travis.pub
-
+chmod 600 id_travis.pub.pem
+ssh-add id_travis.pub.pem
 
 npm install bower
 bower install
