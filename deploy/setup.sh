c05e58edecee942e0c829ba57b232772fa28b671
Ben Brunton <ben@flashtalking.com>
2013-09-13 22:05:36 +0100

    oauth settings

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -2,9 +2,5 @@
 
 echo "setting up Hollywood Hacking - build # ""$TRAVIS_BUILD_NUMBER""\n"
 
-echo "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-chmod 600 id_travis.pub.pem
-ssh-add id_travis.pub.pem
-
 npm install bower
 bower install
