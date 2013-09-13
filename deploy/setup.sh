5642196c8cb9c71e857cdaa4839b42b087080b59
Ben Brunton <ben@flashtalking.com>
2013-09-14 00:51:08 +0100

    tsc needs to start fucking working

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,11 +3,11 @@
 echo "setting up Hollywood Hacking - build # ""$TRAVIS_BUILD_NUMBER""\n"
 
 npm install bower
-npm install -g typescript
+npm install typescript
 bower install
 
 cd script/app/
 echo "--module amd" > ts-files.txt
 find **/*.ts >> ts-files.txt
-tsc @ts-files.txt
+node tsc @ts-files.txt
 rm ts-files.txt
