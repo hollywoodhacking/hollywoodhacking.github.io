211fe78e3b79fde685167c235c321fdcae9d0472
Ben Brunton <ben@flashtalking.com>
2013-09-14 00:23:47 +0100

    working on full commit

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,7 +5,7 @@ echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 git config user.email "travis@travis-ci.org"
 git config user.name $GH_TOKEN
 rm .gitignore
-git add .
+git add . --all
 git commit -m "travis build ${TRAVIS_BUILD_NUMBER}"
 git remote add deploy https://github.com/benbrunton/benbrunton.github.io.git
 git push --force deploy master
