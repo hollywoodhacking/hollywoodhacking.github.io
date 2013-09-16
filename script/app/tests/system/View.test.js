2a276236617c7a572b67089d6d2eb2ca8b91c276
benbrunton <ben@flashtalking.com>
2013-09-16 12:51:28 +0100

    remove all js

--- script/app/tests/system/View.test.js
+++
@@ -1,8 +0,0 @@
-/// <reference path="../jasmine.d.ts" />
-/// <reference path="../../AppDefinitions.d.ts"/>
-define(["require", "exports", 'system/View'], function(require, exports, __View__) {
-    var View = __View__;
-
-    describe('View : ', function () {
-    });
-});
