d688c5b5e51c2fa4ee923cf10f0ebf752b55a419
benbrunton <ben@flashtalking.com>
2013-09-16 11:38:19 +0100

    tsc -g

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,13 +3,13 @@
 echo "setting up Hollywood Hacking - build # ""$TRAVIS_BUILD_NUMBER""\n"
 
 npm install bower
-npm install typescript
+npm install -g typescript
 bower install
 
 cd script/app/
 echo "--module amd" > ts-files.txt
 find **/*.ts >> ts-files.txt
 echo "begin typescript compilation"
-node tsc @ts-files.txt
+tsc @ts-files.txt
 rm ts-files.txt
 echo "typescript compilation complete"
