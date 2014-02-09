057a3a0e618e37751d48507368fdea8aae39ca40
Ben Brunton <ben@flashtalking.com>
2014-02-09 22:07:34 +0000

    started on fullscreen prompt

--- script/app/AppView.ts
+++ script/app/AppView.ts
@@ -9,7 +9,17 @@ class AppView extends AbstractView implements ContainerView{
 
   constructor(selector){
     super(selector);
-    this.$('#splash .close').click(this.toggleSplash)
+    this.$('#splash .close').click(this.toggleSplash);
+
+    if(document.fullscreen || document.mozFullScreen || document.webkitIsFullScreen || document.msFullscreenElement){
+      this.hideFullscreenPrompt();
+    }
+    this.$('#fullscreen_prompt .dismiss').click(this.hideFullscreenPrompt);
+    document.addEventListener("fullscreenchange", this.hideFullscreenPrompt, false);
+    document.addEventListener("fullscreen", this.hideFullscreenPrompt, false);
+    document.addEventListener("mozfullscreenchange", this.hideFullscreenPrompt, false);
+    window.addEventListener("webkitfullscreenchange", this.hideFullscreenPrompt, false);
+    document.addEventListener("msfullscreenchange", this.hideFullscreenPrompt, false);
   }
 
   public attachMainView(view:View){
@@ -26,6 +36,10 @@ class AppView extends AbstractView implements ContainerView{
 
   public toggleSplash(){
     $('#splashHolder').toggle();
+  }
+
+  private hideFullscreenPrompt(){
+    $('#fullscreen_prompt').hide();
   }
 
 }
