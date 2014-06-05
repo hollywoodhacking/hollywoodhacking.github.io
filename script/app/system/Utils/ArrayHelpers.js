308e841715fb5fd7b73b7c68a75cf18f99e96015
958ceedc290534e42bfb0b04b13b64ff7a504712 <travis@travis-ci.org>
2014-06-05 15:07:22 +0000

    travis build 92

---
+++ script/app/system/Utils/ArrayHelpers.js
@@ -0,0 +1,16 @@
+/// <reference path="../../AppDefinitions.d.ts"/>
+define(["require", "exports"], function(require, exports) {
+    var ArrayHelpers = (function () {
+        function ArrayHelpers() {
+        }
+        ArrayHelpers.getRandomItem = function (arr) {
+            var randomIndex = Math.round(Math.random() * (arr.length - 1));
+
+            return arr[randomIndex];
+        };
+        return ArrayHelpers;
+    })();
+
+    
+    return ArrayHelpers;
+});
