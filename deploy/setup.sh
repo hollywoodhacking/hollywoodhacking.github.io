d030c216df16b8201cb0192baea22a378c6bccbb
Ben Brunton <benbrunton@hotmail.com>
2014-12-11 11:14:21 +0000

    remove dep on bower

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,10 +3,10 @@
 echo "setting up Hollywood Hacking - build # ""$TRAVIS_BUILD_NUMBER""\n"
 
 sudo apt-get install zsh
-npm install bower
+#npm install bower
 npm install -g typescript
 npm install -g requirejs
 npm install -g karma
-bower install
+#bower install
 
 deploy/compile.sh
